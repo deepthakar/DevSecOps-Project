09e6877e6b0d471df9f68036af6cb7fb4a57c136
deepthakar <ubuntu@ip-172-31-68-119.ec2.internal>
2026-07-17 14:13:36 +0000

    Update Jenkins conf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "personal-terraform-backend-bucket"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     encrypt      = true
